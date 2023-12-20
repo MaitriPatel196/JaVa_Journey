ba3af6b6cfb95f1eddac56610dd8ba49676b8302
MaitriPatel196 <64016688+MaitriPatel196@users.noreply.github.com>
2023-12-20 15:01:40 -0500

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,11 @@
+public class p1
+{
+	public static void main(String[] args)
+	{
+		int x= 2;
+		int y= 5;
+		String Fname= "Maitri";
+		System.out.println(x + y);
+		System.out.println(Fname);
+	}
+}
